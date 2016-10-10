1e9bf70799e0d2759825bb86edb2272a0756c5c1
vikram <vikram.lapenatech@gmail.com>
2016-10-10 18:42:20 +0530

    [As a Developer, I want to Create a script that can Get user Input and process #1] facebook link open

---
+++ project/facebook_api.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from urllib2 import Request
+import urllib2, re
+
+class url(object):
+
+	def __init__(self):
+		self.geturl()
+
+	def geturl(self):
+		url = raw_input('Enter the URL::')
+		req = Request(url)
+
+		html_page = urllib2.urlopen(req)
+		soup = BeautifulSoup(html_page,"lxml")
+		for link in soup.findAll('a'):
+			uurl = link.get('href')
+			try:
+				if "facebook.com" in uurl:
+					print uurl
+			except:
+				pass
+
+if __name__=="__main__":
+	obj =url()
